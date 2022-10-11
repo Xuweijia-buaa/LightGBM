e08db3a0ce5c8c1d905eac1a6357032670d44da3
Yu Shi <shiyu_k1994@qq.com>
2022-10-11 07:43:12 +0000

    install azcopy

--- prepare_distributed.sh
+++ prepare_distributed.sh
@@ -8,5 +8,9 @@ cmake .. -DUSE_TIMETAG=ON
 make -j
 cd ..
 cd ..
+wget https://azcopyvnext.azureedge.net/release20220315/azcopy_linux_amd64_10.14.1.tar.gz
+tar xvzf azcopy_linux_amd64_10.14.1.tar.gz
+echo "export PATH=\$PATH:$PWD/azcopy_linux_amd64_10.14.1/" >> ~/.bashrc
+source ~/.bashrc
 azcopy copy "https://fastbertjp.blob.core.windows.net/shiyu/lgb_data/epsilon20X.train?sv=2020-10-02&st=2022-10-11T06%3A32%3A20Z&se=2022-10-12T06%3A32%3A20Z&sr=b&sp=r&sig=awAaT1AZJmbNQcErt4AEyvx8a1GpEoQre%2Bw0csvyH88%3D" .
 azcopy copy "https://fastbertjp.blob.core.windows.net/shiyu/lgb_data/epsilon.test?sv=2020-10-02&st=2022-10-11T06%3A33%3A04Z&se=2022-10-12T06%3A33%3A04Z&sr=b&sp=r&sig=RAShdxnbnapUtvAMK8wWrjyZncftFNJwSX4koLS9by8%3D" .
