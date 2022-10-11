370b746f5a0dc2207922662778ec8289f6fa68e1
Yu Shi <shiyu_k1994@qq.com>
2022-10-11 07:26:42 +0000

    add prepare scripts

---
+++ prepare_distributed.sh
@@ -0,0 +1,12 @@
+git clone --recursive https://github.com/shiyu1994/LightGBM.git
+cd LightGBM
+git checkout cuda-discretized-nccl3
+git submodule update --init
+mkdir build
+cd build
+cmake .. -DUSE_TIMETAG=ON
+make -j
+cd ..
+cd ..
+azcopy copy "https://fastbertjp.blob.core.windows.net/shiyu/lgb_data/epsilon20X.train?sv=2020-10-02&st=2022-10-11T06%3A32%3A20Z&se=2022-10-12T06%3A32%3A20Z&sr=b&sp=r&sig=awAaT1AZJmbNQcErt4AEyvx8a1GpEoQre%2Bw0csvyH88%3D" .
+azcopy copy "https://fastbertjp.blob.core.windows.net/shiyu/lgb_data/epsilon.test?sv=2020-10-02&st=2022-10-11T06%3A33%3A04Z&se=2022-10-12T06%3A33%3A04Z&sr=b&sp=r&sig=RAShdxnbnapUtvAMK8wWrjyZncftFNJwSX4koLS9by8%3D" .
