0b3b057fd2efb90deb64e5b0830f7af308c782eb
Yu Shi <shiyu_k1994@qq.com>
2022-10-15 05:08:53 +0000

    add criteo

---
+++ prepare_distributed_criteo.sh
@@ -0,0 +1,2 @@
+azcopy copy "https://pretrain.blob.core.windows.net/quantized-gbdt/dataset/criteo.train?sv=2020-04-08&st=2022-10-15T04%3A52%3A21Z&se=2022-10-16T04%3A52%3A21Z&sr=b&sp=r&sig=473zBx65V7Zjx18BmfDp%2BadiLo%2ByF%2F3uk1fhgcIlqhc%3D" .
+azcopy copy "https://pretrain.blob.core.windows.net/quantized-gbdt/dataset/criteo.test?sv=2020-04-08&st=2022-10-15T04%3A53%3A01Z&se=2022-10-16T04%3A53%3A01Z&sr=b&sp=r&sig=5hL0MPPT6GNSPP20Q5TobGxpNvACFTypp010CIa8YW4%3D" .
